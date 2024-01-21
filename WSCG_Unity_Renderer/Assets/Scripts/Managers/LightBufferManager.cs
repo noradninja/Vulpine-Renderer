840eab881118162661299c4ab6e691f8df093e35
Noradninja <josh@whitespacecg.com>
2024-01-21 12:52:44 -0500

    First code push

---
+++ WSCG_Unity_Renderer/Assets/Scripts/Managers/LightBufferManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WSCG
+{
+	public class LightBufferManager : MonoBehaviour
+	{
+		public Light lights[];
+		public Vector4 lightPosX;
+		public Vector4 lightPosY;
+		public Vector4 lightPosZ;
+		
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
