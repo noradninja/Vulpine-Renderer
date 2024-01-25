ea683e3d21df6ebb0d517b3714a36f0e7a18969e
Noradninja <josh@whitespacecg.com>
2024-01-25 01:55:04 -0500

    LM- 90% LBM- 50%

--- WSCG_Unity_Renderer/Assets/Scripts/Managers/LightBufferManager.cs
+++ WSCG_Unity_Renderer/Assets/Scripts/Managers/LightBufferManager.cs
@@ -1,107 +1,125 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Rendering;
+﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace WSCG
 {
-	public class LightBufferManager : MonoBehaviour
-	{
-		//set max light count onscreen, create array to hold lights
-		//lights will add themselves to the array, based on if the AABB of their range falls within the screen
-		
-		private static int maxLights = 4;
-		public Light[] lights = new Light[maxLights];
-		
-		//vectors to pack position for four lights
-		public Vector4 lightPosX;
-		public Vector4 lightPosY;
-		public Vector4 lightPosZ;
-		//we are packing range for four lights in w for our shader to use, so we store it here to pass along
-		public Vector4 lightPosW;
-		
-		//vectors to pack rgb color values for four lights
-		public Vector4 lightColorR;
-		public Vector4 lightColorG;
-		public Vector4 lightColorB;
-		//we are packing intensity for four lights in w for our shader to use, so we store it here to pass along
-		public Vector4 lightColorA;
-		
-		//GPU buffer
-		private ComputeBuffer lightingDataBuffer;
-		 
-		void Start() //grab all our light data for passing to the structured buffer
-		{
-			
-			//XYZ light positions for four lights, packed by axis 
-			lightPosX = new Vector4(lights[0].transform.position.x,
-				                  lights[1].transform.position.x,
-				                  lights[2].transform.position.x,
-				                  lights[3].transform.position.x);
-			lightPosY = new Vector4(lights[0].transform.position.y,
-									  lights[1].transform.position.y,
-								    lights[2].transform.position.y,
-									lights[3].transform.position.y);
-			lightPosZ = new Vector4(lights[0].transform.position.z,
-									lights[1].transform.position.z,
-									  lights[2].transform.position.z,
-									lights[3].transform.position.z);
-			//range for four lights, packed
-			lightPosW= new Vector4(lights[0].range,
-								   lights[1].range,
-								   lights[2].range,
-								  lights[3].range);
-			//RGB light colors for four lights, packed by channel 
-			lightColorR	 = new Vector4(lights[0].color.r,
-									   lights[1].color.r,
-									   lights[2].color.r,
-									   lights[3].color.r);
-			lightColorG = new Vector4(lights[0].color.g,
-									  lights[1].color.g,
-									  lights[2].color.g,
-									  lights[3].color.g);
-			lightColorB = new Vector4(lights[0].color.b,
-									  lights[1].color.b,
-									  lights[2].color.b,
-									 lights[3].color.b);
-			//intensity for four lights, packed
-			lightColorA = new Vector4(lights[0].intensity,
-									  lights[1].intensity,
-									  lights[2].intensity,
-									 lights[3].intensity);
+    public class LightBufferManager : MonoBehaviour
+    {
+        //set max light count onscreen, create array to hold lights
+        //lights will add themselves to the array, based on if the bounds of their range falls within the screen,
+        //see LightManager.cs
 
-			lightingDataBuffer = new ComputeBuffer(maxLights, LightingData.GetSize());
-		}
-		// Update is called once per frame
-		void Update() 
-		{
-			LightingData[] lightData;
-			lightData = new LightingData[0];
-			lightingDataBuffer.SetData(lightData);
-		}
+        public static int MaxLights = 4;
+        public static Vector4 usedSlots = Vector4.zero;
+        public static Light[] Lights = new Light[MaxLights];
+        
+        //vectors to pack position for four lights 
+        public Vector4 lightPosX;
+        public Vector4 lightPosY;
+        public Vector4 lightPosZ;
+        //we are packing range for four lights in w 
+        public Vector4 lightPosW;
 
-		public struct LightingData
-		{
-			//vectors to pack position for four lights
-			public Vector4 lightPosX;
-			public Vector4 lightPosY;
-			public Vector4 lightPosZ;
-			//we are packing range for four lights in w for our shader to use, so we store it here to pass along
-			public Vector4 lightPosW;
-		
-			//vectors to pack rgb color values for four lights
-			public Vector4 lightColorR;
-			public Vector4 lightColorG;
-			public Vector4 lightColorB;
-			//we are packing intensity for four lights in w for our shader to use, so we store it here to pass along
-			public Vector4 lightColorA;
-			
-			public static int GetSize()
-			{
-				//we return float size because Vector4 size isn't safe, so 4 floats * 8 Vectors
-				return sizeof(float) * 4 * 8;
-			}
-		}
-		
-	}
+        //vectors to pack rgb color values for four lights
+        public Vector4 lightColorR;
+        public Vector4 lightColorG;
+        public Vector4 lightColorB;
+        //we are packing intensity for four lights in w 
+        public Vector4 lightColorA;
+
+        //Lighting data structure
+        private LightingData[] _lightData;
+
+        //GPU data buffer
+        private ComputeBuffer _lightingDataBuffer;
+
+        private void Start()
+        {
+            //grab all our light data for passing to the GPU structured buffer
+            
+            //XYZ light positions for four lights, packed by axis 
+            lightPosX = new Vector4(
+                Lights[0].transform.position.x,
+                Lights[1].transform.position.x,
+                Lights[2].transform.position.x,
+                Lights[3].transform.position.x
+            );
+            lightPosY = new Vector4(
+                Lights[0].transform.position.y,
+                Lights[1].transform.position.y,
+                Lights[2].transform.position.y,
+                Lights[3].transform.position.y
+            );
+            lightPosZ = new Vector4(
+                Lights[0].transform.position.z,
+                Lights[1].transform.position.z,
+                Lights[2].transform.position.z, 
+                Lights[3].transform.position.z
+            );
+            //range for four lights, packed
+            lightPosW = new Vector4(
+                Lights[0].range,
+                Lights[1].range,
+                Lights[2].range,
+                Lights[3].range
+            );
+            //RGB light colors for four lights, packed by channel 
+            lightColorR = new Vector4(
+                Lights[0].color.r,
+                Lights[1].color.r,
+                Lights[2].color.r,
+                Lights[3].color.r
+            );
+            lightColorG = new Vector4(Lights[0].color.g,
+                Lights[1].color.g,
+                Lights[2].color.g,
+                Lights[3].color.g
+            );
+            lightColorB = new Vector4(
+                Lights[0].color.b,
+                Lights[1].color.b,
+                Lights[2].color.b,
+                Lights[3].color.b
+            );
+            //intensity for four lights, packed
+            lightColorA = new Vector4(
+                Lights[0].intensity,
+                Lights[1].intensity,
+                Lights[2].intensity,
+                Lights[3].intensity
+            );
+
+            _lightingDataBuffer = new ComputeBuffer(MaxLights, LightingData.GetSize());
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            _lightData = new LightingData[1];
+            _lightingDataBuffer.SetData(_lightData);
+        }
+
+        private struct LightingData
+        {
+            //vectors to pack position for four lights
+            public Vector4 LightPosX;
+            public Vector4 LightPosY;
+            public Vector4 LightPosZ;
+            //we are packing range for four lights in w
+            public Vector4 LightPosW;
+
+            //vectors to pack rgb color values for four lights
+            public Vector4 LightColorR;
+            public Vector4 LightColorG;
+            public Vector4 LightColorB;
+            //we are packing intensity for four lights in w
+            public Vector4 LightColorA;
+
+            //we return sizeof(float) because sizeof(Vector4) is unsafe. 4 floats * 8 Vectors = 32 floats * 4 bytes/float = 128 bytes/frame
+            public static int GetSize()
+            {
+                return sizeof(float) * 4 * 8;
+            }
+        }
+    }
 }
---
+++ WSCG_Unity_Renderer/Assets/Scripts/Managers/LightManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WSCG
+{
+	[RequireComponent(typeof(Light))]
+	public class LightManager : MonoBehaviour
+	{
+		//flow control
+		private bool _isInBuffer = false;
+		private bool _isInCamera = false;
+	
+		//light
+		private Light _lightObject;
+		private int _thisLightSlot;
+		private Bounds _lightBounds;
+		private Vector3 _lightCenter;
+		private float _lightRadius;
+		
+		//camera
+		private Camera _mainCam;
+		private Vector3 _cameraViewportPoint;
+		private float _distanceToCamera;
+		
+		// Use this for initialization
+		private void Start()
+		{
+			//cache refs for camera and light
+			_mainCam = Camera.main;
+			_lightObject = GetComponent<Light>();
+			//check if light volume is in the camera view
+			CheckCameraView(_lightObject);
+		}
+		
+		// ReSharper disable once Unity.RedundantEventFunction
+		private void Update()
+		{
+			/*
+				-have an incrementor
+				-on framerate%increment = 0, fire cam check
+				-this gives us variable frequency [30/increment = int(fps)] for checking if we are in the camera view			
+			*/
+		}
+
+		private void CheckCameraView(Light lightToCheck)
+		{
+			//grab our bounds and center, extract radius from bounds
+			_lightBounds = lightToCheck.GetComponent<Renderer>().bounds;
+			_lightCenter = _lightBounds.center;
+			_lightRadius = _lightBounds.extents.magnitude;
+			//get a point from the camera to the viewport center to check against, get distance to the light's center point
+			_cameraViewportPoint = _mainCam.ViewportToWorldPoint( new Vector3(0.5f, 0.5f, _mainCam.nearClipPlane) );
+			//find the distance from the light center to the camera center
+			_distanceToCamera = Vector3.Distance(_lightCenter, _cameraViewportPoint);
+			
+			//check if camera distance is within light radius, toggle bool
+			if (_distanceToCamera <= _lightRadius)
+			{
+				_isInCamera = true;
+				AddLight(lightToCheck);
+			}
+			else
+			{
+				_isInCamera = false;
+				RemoveLight(lightToCheck);
+			}
+		}
+		
+		private void AddLight(Light lightToAdd)
+		{
+			if (_isInBuffer)
+				Debug.Log("Light is already in buffer in slot " + _thisLightSlot);
+			else
+			{
+				if (LightBufferManager.usedSlots != Vector4.one) //does the buffer contain an empty slot?
+				{
+					for (int l = 0; l < LightBufferManager.MaxLights - 1; l++) //spin slot vector components
+					{
+						if (LightBufferManager.usedSlots[l] == 0) //the spot is empty
+						{
+							_isInBuffer = true; //toggle the boolean
+							_thisLightSlot = l; //set our value to l value for use in removal
+							LightBufferManager.Lights[_thisLightSlot] = _lightObject; //add this light to object list
+							LightBufferManager.usedSlots[_thisLightSlot] = 1; //set the Vec4 component to 1 so it is 'used'
+							Debug.Log("Light added to buffer in slot " + _thisLightSlot);
+							Debug.Log("Available slots: " + LightBufferManager.usedSlots.ToString());
+						}
+					}
+				}
+				else
+				{
+					Debug.Log("All slots are occupied: " + LightBufferManager.usedSlots.ToString());
+					/*
+						-If all the slots are full, we should spin through them and check the _distanceToCamera, and if our
+						_distanceToCamera is less than the largest value (furthest light), replace that light with this one;
+						otherwise, we are the furthest light and should be skipped till the next check happens
+					 */
+				}
+			}
+		}
+
+		private void RemoveLight(Light lightToRemove)
+		{
+			if (!_isInBuffer)
+			{
+				Debug.Log("Light not in buffer!");
+			}
+			else
+			{
+				_isInBuffer = false; //toggle the boolean
+				LightBufferManager.Lights[_thisLightSlot] = null; //remove this light from object list
+				LightBufferManager.usedSlots[_thisLightSlot] = 0; //set the Vec4 component to 0.0 so it is 'empty'
+				Debug.Log("Light removed from buffer slot " + _thisLightSlot);
+				Debug.Log("Available slots: " + LightBufferManager.usedSlots.ToString());
+			}
+		}
+	}
+}
